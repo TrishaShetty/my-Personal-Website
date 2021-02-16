15d81d37554c087795de935246ee590becc581b3
Trisha <trisha@Admins-MacBook-Pro.local>
2021-02-16 08:12:21 +0530

    Home page done, with carousal, title message, type writer and scroll down button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 import MyNavBar from './components/my-nav-bar/my-nav-bar.component.jsx';
+import MyCarousal from './components/my-carousal/my-carousal.component.jsx';
+import TitleMessage from './components/title-message/title-message.component.jsx';
+import ScrollDown from './components/scroll-down/scroll-down.component.jsx';
 
 import './App.css';
 
 const App = () => {
   return (<div>
     <MyNavBar />
-    My Personal Website!
+    <MyCarousal />
+    <TitleMessage />
+    <ScrollDown />
   </div>)
 }
 
---
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+import Slide1 from '../../assets/img/carousal/slide1.webp';
+import Slide2 from '../../assets/img/carousal/slidex.webp';
+import Slide3 from '../../assets/img/carousal/slide3.webp';
+
+import './my-carousal.styles.css';
+
+const MyCarousal = () => {
+    return(
+        <div id="home">
+            <Carousel controls={false} indicators interval={2500} pause={false}>
+            <Carousel.Item>
+                <img
+                className="d-block w-100 .custom-img"
+                src={Slide1}
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <p>Nothing will work unless you do.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100 .custom-img"
+                src={Slide2}
+                alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                <p>To handle yourself, use your head; to handle others, use your heart.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100 .custom-img"
+                src={Slide3}
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <p>Do or do not. There is no try.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel>
+        </div>
+    )
+}
+
+export default MyCarousal;
