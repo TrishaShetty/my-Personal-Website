136c0545070ff40cb900db882c5f4bf3018fe52f
Trisha <trisha@Admins-MacBook-Pro.local>
2021-02-18 08:15:56 +0530

    about me completed, with Parallex and Fade

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from "react";
+import { Parallax } from "react-parallax";
+import Fade from 'react-reveal/Fade';
+import Container from "react-bootstrap/Container";
 
 import MyNavBar from './components/my-nav-bar/my-nav-bar.component.jsx';
 import MyCarousal from './components/my-carousal/my-carousal.component.jsx';
 import TitleMessage from './components/title-message/title-message.component.jsx';
 import ScrollDown from './components/scroll-down/scroll-down.component.jsx';
+import AboutMe from './pages/about-me/about-me.component.jsx';
 
 import './App.css';
 
 const App = () => {
-  return (<div>
+  return (<div className="App" style={{ position: "relative" }}>
     <MyNavBar />
     <MyCarousal />
     <TitleMessage />
     <ScrollDown />
-  </div>)
-}
+    <div>
+      <Parallax blur={{ min: -15, max: 15 }} bgImage={require("./assets/img/parallex/background.webp")} bgImageAlt="abc" strength={-200}>
+        <div>
+          <Container className="container-box rounded">
+            {/* <Fade duration={500}> */}
+              <AboutMe />
+            {/* </Fade> */}
+          </Container>
+        </div>
+      </Parallax>
+    </div>
+  </div>);
+};
 
 export default App;
