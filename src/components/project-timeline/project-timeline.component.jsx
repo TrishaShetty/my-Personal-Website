f6e103ee128d5b10e6aeff0a6ac926c52eba004f
Trisha <trisha@Admins-MacBook-Pro.local>
2021-02-20 10:37:44 +0530

    moving to a diff repo, will be back

---
+++ src/components/project-timeline/project-timeline.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+
+import './project-timeline.styles.css';
+
+const ProjectTimeline = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ProjectTimeline;
